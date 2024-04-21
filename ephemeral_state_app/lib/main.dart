5e6c7db2873e52825b181f106cff187d2813f31c
adityaashinde <adityashinde4580@gmail.com>
2024-04-22 00:05:21 +0530

    SetState Management

--- ephemeral_state_app/lib/main.dart
+++ ephemeral_state_app/lib/main.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 
+/// for the use of log() method
+import 'dart:developer';
+
+import 'package:flutter/widgets.dart';
+
 void main() {
   runApp(const MainApp());
 }
@@ -10,11 +15,101 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+      debugShowCheckedModeBanner: false,
+      home: EphemeralStateDemo(),
+    );
+  }
+}
+
+class EphemeralStateDemo extends StatefulWidget {
+  const EphemeralStateDemo({super.key});
+
+  @override
+  State<EphemeralStateDemo> createState() => _SetStateManagement();
+}
+
+class _SetStateManagement extends State<EphemeralStateDemo> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey.shade400,
+      appBar: AppBar(
+        backgroundColor: Colors.purpleAccent.shade400,
+        centerTitle: true,
+        title: const Text(
+          "SetState Management Demo",
+          style: TextStyle(
+            fontWeight: FontWeight.w600,
+            fontSize: 18,
+          ),
         ),
       ),
+      body: const Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Course(
+            courseName: "Java",
+          ),
+          SizedBox(
+            height: 50,
+          ),
+          Course(
+            courseName: "Flutter",
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class Course extends StatefulWidget {
+  final String courseName;
+  const Course({super.key, required this.courseName});
+
+  @override
+  State createState() => _CourseState();
+}
+
+class _CourseState extends State<Course> {
+  int courseCount = 0;
+  @override
+  Widget build(BuildContext context) {
+    /// avoid print in production code use log() method.
+    log("In CourseState build");
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        GestureDetector(
+          onTap: () {
+            setState(() {
+              courseCount++;
+            });
+          },
+          child: Container(
+            height: 80,
+            width: 100,
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+              color: Colors.orange.shade300,
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: Text(widget.courseName),
+          ),
+        ),
+        const SizedBox(
+          width: 50,
+        ),
+        Container(
+          height: 80,
+          width: 100,
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            color: Colors.green.shade300,
+            borderRadius: BorderRadius.circular(20),
+          ),
+          child: Text("Count : $courseCount"),
+        ),
+      ],
     );
   }
 }
