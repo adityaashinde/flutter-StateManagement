8efe4d8fb236fb508388a3779612bbcfcc284acf
adityaashinde <adityashinde4580@gmail.com>
2024-04-27 09:44:11 +0530

    Provider State Management

--- provider_app_demo/lib/main.dart
+++ provider_app_demo/lib/main.dart
@@ -1,4 +1,7 @@
+// provider state management example
+
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,12 +12,54 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return Provider(
+      create: (context) {
+        return Company(companyName: "Google", empCount: 250);
+      },
+      child: const MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: MyApp(),
       ),
     );
   }
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State createState() => _MainAppState();
+}
+
+class _MainAppState extends State {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.purpleAccent,
+        title: const Text("Provider State Management Demo"),
+        centerTitle: true,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(Provider.of<Company>(context).companyName),
+              const SizedBox(width: 30),
+              Text("${Provider.of<Company>(context).empCount}"),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class Company {
+  String companyName;
+  int empCount;
+
+  Company({required this.companyName, required this.empCount});
 }
